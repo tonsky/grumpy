e2cbdfb6a00b35c3751f51945045cfa9e12c4c97
Nikita Prokopov <prokopov@gmail.com>
2017-09-05 14:09:17 +0300

    do not repeat subtitles twice

--- resources/static/scripts.js
+++ resources/static/scripts.js
@@ -1,6 +1,5 @@
-function reload_subtitle() {
-  var subtitles = [
-    'Отменить изменения? – Отменить / Отмена', 
+var subtitles =
+  [ 'Отменить изменения? – Отменить / Отмена', 
     'Select purchase to purchase for $0.00 – PURCHASE / CANCEL', 
     'Это не текст, это ссылка. Не нажимайте на ссылку.',
     'Не обновляйте эту страницу! Не нажимайте НАЗАД',
@@ -17,17 +16,21 @@ function reload_subtitle() {
     'Update Java Runtime?',
     'Компьютер будет перезагружен через 15 минут',
     'grumpy.website запрашивает разрешение на: Показывать оповещения',
-    'Ваш браузер устарел'
-  ];
-  var subtitle = subtitles[Math.floor(Math.random() * subtitles.length)];
-  var div = document.querySelector('.subtitle > span');
-  div.innerHTML = subtitle;
+    'Ваш браузер устарел' ],
+  subtitle_el = document.querySelector('.subtitle > span');
+
+
+function reload_subtitle() {
+  do {
+    var subtitle = subtitles[Math.floor(Math.random() * subtitles.length)];
+  } while (subtitle === subtitle_el.innerText);
+  subtitle_el.innerHTML = subtitle;
 }
 
 
 window.addEventListener("load", function() {
+  subtitle_el.onclick = reload_subtitle;
   reload_subtitle();
-  document.querySelector('.subtitle > span').onclick = reload_subtitle;
   if (document.cookie.indexOf("grumpy_user=") >= 0) {
     document.body.classList.remove("anonymous");
   }

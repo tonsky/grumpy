6cfa4b5165e9d0c3967e73de473fad0c6917c452
Nikita Prokopov <niki@tonsky.me>
2020-02-23 01:10:54 +0100

    More editor states

--- src/grumpy/editor.cljs
+++ src/grumpy/editor.cljs
@@ -31,29 +31,38 @@
 
       [:.textarea
        [:.input
-        [:textarea {:placeholder "Be grumpy here..." :default-value body}]]
+        [:textarea {:placeholder "Be grumpy here..." :value body}]]
        [:.handle.column.center
         [:.rope]
         [:.ring.cursor-pointer]]]]
 
-     [:button.post-post.row
-      [:img.button {:src "/static/editor/post_button.svg"}]
-      [:img.hand {:src "/static/editor/post_hand.svg"}]
-      [:.label (if (some? id) "Update" "POST")]]
+     (if (nil? id)
+       [:button.post-post.row
+        [:img.button {:src "/static/editor/post_button.svg"}]
+        [:img.hand {:src "/static/editor/post_hand.svg"}]
+        [:.label "POST"]]
+       [:button.btn.post-update "Update"])
 
-     [:button.post-delete.btn.self-middle
-       (if (some? id) "Cancel edit" "Delete draft")]]))
+     (if (nil? id)
+       [:button.post-delete.btn.self-middle.self-right "Delete draft"]
+       [:button.post-cancel.btn.self-right "Cancel edit"])]))
 
 
 (def debug-states
   [["New"              {}]
+   ["Edit"             {:post {:id "0THGrh25y" :body "Livejournal mobile icons. My interpretation:\n\n- A guy was thinking about doing some writing. About heavy machinery, I suppose.\n- Nobody would publish his paper.\n- He brought two friends.\n- They wrote a book.\n- It gets published.\n- They receive a gift.\n- He starts thinking about next book, but one of the friends didn’t like it, so he left.\n- Some time later, second friend leaves too. He’s all alone again." :picture {:url "/post/0THGrh25y/M0C_mT1.fit.jpeg", :content-type "image/jpeg", :dimensions [750 265]}}}]
    ["Another author"   {:post {:author "freetonik"}}]
-   ["Body changed"     {:post {:body "Hello, world!"}}]
-   ["Dragged"          {}]
-   ["Sending"          {:post {:picture {:url "/post/0THGrh25y/M0C_mT1.orig.png"}}}]
-   ["Sending vertical" {:post {:picture {:url "/post/0TDdxMGm8/M-Jw_2o.orig.png"}}}]
-   ["Image uploaded"   {:post {:picture {:url "/post/0THGrh25y/M0C_mT1.fit.jpeg", :content-type "image/jpeg", :dimensions [750 265]}}}]
-   ["Edit"             {:post {:id "0THGrh25y"}}]])
+   ["Body changed"     {:post {:body "Hello, world!"} :status/body :status.body/changed}]
+   ["Body saving"      {:post {:body "Hello, world!"} :status/body :status.body/saving}]
+   ["Body failed"      {:post {:body "Hello, world!"} :status/body :status.body/failed :status.body/message "Autosave failed"}]
+   ["Dragged"          {:dragged? true}]
+   ["Dragged over image" {:dragged? true :post {:picture {:url "/post/0THGrh25y/M0C_mT1.orig.png"}}}]
+   ["Uploading 0%"       {:blob/url "/post/0THGrh25y/M0C_mT1.orig.png" :status/picture :status.picture/uploading :status.picture/progress 0}]
+   ["Uploading 50%"      {:blob/url "/post/0THGrh25y/M0C_mT1.orig.png" :status/picture :status.picture/uploading :status.picture/progress 0.5}]
+   ["Uploaded"         {:post {:picture {:url "/post/0THGrh25y/M0C_mT1.fit.jpeg", :content-type "image/jpeg", :dimensions [750 265]}}}]
+   ["Uploaded vertical" {:post {:picture {:url "/post/0TDdxMGm8/M-Jw_2o.fit.jpeg", :content-type "image/jpeg", :dimensions [638 922]}}}]
+   ["Upload failed"    {:blob/url "/post/0THGrh25y/M0C_mT1.orig.png" :status/picture :status.picture/failed :status.picture/message "500 Internal Server Error"}]
+   ])
 
 
 (defonce *debug-state-key (atom "New"))

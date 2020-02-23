b6368b1236567d12119300dc8bc64e7379a4fd82
Nikita Prokopov <niki@tonsky.me>
2020-02-23 16:14:37 +0100

    Implemented layout for all editor states

--- src/grumpy/editor.cljs
+++ src/grumpy/editor.cljs
@@ -3,66 +3,143 @@
    [clojure.string :as str]
    [grumpy.core.coll :as coll]
    [grumpy.core.fragments :as fragments]
-   [grumpy.core.macros :refer [oget oset! js-fn]]
+   [grumpy.core.macros :refer [oget oset! js-fn cond+]]
    [rum.core :as rum]))
 
 
 (enable-console-print!)
 
 
-(rum/defc editor [state]
-  (let [{:keys [post]} state
-        {:keys [id body author picture]} post]
-    [:.editor.grid
+(rum/defc no-media [dragged?]
+  [:.upload.no-select.cursor-pointer
+   [:.corner.top-left]
+   [:.corner.top-right]
+   [:.corner.bottom-left]
+   [:.corner.bottom-right]
+   [:.label (if dragged?
+              "DROP IT!"
+              "Drag media here")]])
+
+
+(rum/defc media-uploaded [picture]
+  [:.media
+   [:.media-wrap
+    [:img {:src (:url picture)}]
+    [:.media-delete.cursor-pointer]]])
+
+
+(rum/defc media-uploading [{blob-url :blob/url
+                            progress :status.picture/progress}]
+  (let [percent (-> progress (* 100))]
+    [:.media
+     [:.media-wrap
+      [:img {:src blob-url}]
+      [:.upload-overlay {:style {:height (str (- 100 percent) "%")}}]]
+     [:.status "> Uploading " (js/Math.floor percent) "%"]]))
+
+
+(rum/defc media-failed [{blob-url :blob/url
+                         message :status.picture/message}]
+  [:.media
+   [:.media-wrap
+    [:img {:src blob-url}]
+    [:.media-delete.cursor-pointer]
+    [:.upload-overlay-failed]]
+   [:.status "> Upload failed (" message ") " [:button.inline "↻ Try again"]]])
+
+
+(rum/defc media
+  < {:before-render
+     (fn [state]
+       (let [[arg] (:rum/args state)
+             {:keys [dragged?]} arg
+             set? (js/document.body.classList.contains "dragover")]
+         (when (not= set? dragged?)
+           (if dragged?
+             (js/document.body.classList.add "dragover")
+             (js/document.body.classList.remove "dragover"))))
+       state)}
+  [arg]
+  (let [{{picture :picture} :post
+         dragged?           :dragged?
+         status-picture     :status/picture} arg]
+    (cond+
+      (some? picture)
+      (media-uploaded picture)
+      
+      (= :status.picture/uploading status-picture)
+      (media-uploading arg)
+
+      (= :status.picture/failed status-picture)
+      (media-failed arg)
+
+      :else
+      (no-media dragged?))))
+
+
+(rum/defc buttons-new [arg]
+  [:.row.middle.space-between
+   [:button.post-post.row
+    [:img.button {:src "/static/editor/post_button.svg"}]
+    [:img.hand {:src "/static/editor/post_hand.svg"}]
+    [:.label "POST"]]
+   [:button.post-delete.btn.self-middle.self-right "Delete draft"]])
+
+
+(rum/defc buttons-edit [arg]
+  [:.row.middle.space-between
+   [:button.btn.post-update "Update"]
+   [:button.post-cancel.btn.self-right "Cancel edit"]])
+
+
+(rum/defc editor
+  [arg]
+  (let [{{:keys [id body author]} :post
+         status-body              :status/body} arg]
+    [:.editor.row
      [:img.post_avatar {:src (fragments/avatar-url author)}]
      
-     [:.editing-col.self-hstretch.column
-      (if (some? picture)
-        [:.post_img.post_img-flex
-         [:img {:src (:url picture)}]
-         [:.media-delete.cursor-pointer]
-         [:.status "> Saved"]]
-        [:.upload.no-select.cursor-pointer
-         [:.corner.top-left]
-         [:.corner.top-right]
-         [:.corner.bottom-left]
-         [:.corner.bottom-right]
-         [:.label "Drag media here"]])
+     [:.column.grow
+      (media arg)
+
+      (case status-body
+        :status.body/changed
+        [:div "Body: changed"]
+        :status.body/saving
+        [:div "Body: saving..."]
+        :status.body/failed
+        [:div "Body: failed (" (:status.body/message arg) ")"]
+        nil)
 
       [:.textarea
        [:.input
         [:textarea {:placeholder "Be grumpy here..." :value body}]]
        [:.handle.column.center
         [:.rope]
-        [:.ring.cursor-pointer]]]]
+        [:.ring.cursor-pointer]]]
 
-     (if (nil? id)
-       [:button.post-post.row
-        [:img.button {:src "/static/editor/post_button.svg"}]
-        [:img.hand {:src "/static/editor/post_hand.svg"}]
-        [:.label "POST"]]
-       [:button.btn.post-update "Update"])
-
-     (if (nil? id)
-       [:button.post-delete.btn.self-middle.self-right "Delete draft"]
-       [:button.post-cancel.btn.self-right "Cancel edit"])]))
+      (if (nil? id)
+        (buttons-new arg)
+        (buttons-edit arg))]]))
 
 
 (def debug-states
-  [["New"              {}]
-   ["Edit"             {:post {:id "0THGrh25y" :body "Livejournal mobile icons. My interpretation:\n\n- A guy was thinking about doing some writing. About heavy machinery, I suppose.\n- Nobody would publish his paper.\n- He brought two friends.\n- They wrote a book.\n- It gets published.\n- They receive a gift.\n- He starts thinking about next book, but one of the friends didn’t like it, so he left.\n- Some time later, second friend leaves too. He’s all alone again." :picture {:url "/post/0THGrh25y/M0C_mT1.fit.jpeg", :content-type "image/jpeg", :dimensions [750 265]}}}]
-   ["Another author"   {:post {:author "freetonik"}}]
-   ["Body changed"     {:post {:body "Hello, world!"} :status/body :status.body/changed}]
-   ["Body saving"      {:post {:body "Hello, world!"} :status/body :status.body/saving}]
-   ["Body failed"      {:post {:body "Hello, world!"} :status/body :status.body/failed :status.body/message "Autosave failed"}]
-   ["Dragged"          {:dragged? true}]
+  [["New"                {}]
+   ["Edit"               {:post {:id "0THGrh25y" :body "Livejournal mobile icons. My interpretation:\n\n- A guy was thinking about doing some writing. About heavy machinery, I suppose.\n- Nobody would publish his paper.\n- He brought two friends.\n- They wrote a book.\n- It gets published.\n- They receive a gift.\n- He starts thinking about next book, but one of the friends didn’t like it, so he left.\n- Some time later, second friend leaves too. He’s all alone again." :picture {:url "/post/0THGrh25y/M0C_mT1.fit.jpeg", :content-type "image/jpeg", :dimensions [750 265]}}}]
+   ["Another author"     {:post {:author "freetonik"}}]
+   ["Body changed"       {:post {:body "Hello, world!"} :status/body :status.body/changed}]
+   ["Body saving"        {:post {:body "Hello, world!"} :status/body :status.body/saving}]
+   ["Body failed"        {:post {:body "Hello, world!"} :status/body :status.body/failed :status.body/message "Autosave failed"}]
+   ["Dragged"            {:dragged? true}]
    ["Dragged over image" {:dragged? true :post {:picture {:url "/post/0THGrh25y/M0C_mT1.orig.png"}}}]
    ["Uploading 0%"       {:blob/url "/post/0THGrh25y/M0C_mT1.orig.png" :status/picture :status.picture/uploading :status.picture/progress 0}]
    ["Uploading 50%"      {:blob/url "/post/0THGrh25y/M0C_mT1.orig.png" :status/picture :status.picture/uploading :status.picture/progress 0.5}]
-   ["Uploaded"         {:post {:picture {:url "/post/0THGrh25y/M0C_mT1.fit.jpeg", :content-type "image/jpeg", :dimensions [750 265]}}}]
-   ["Uploaded vertical" {:post {:picture {:url "/post/0TDdxMGm8/M-Jw_2o.fit.jpeg", :content-type "image/jpeg", :dimensions [638 922]}}}]
-   ["Upload failed"    {:blob/url "/post/0THGrh25y/M0C_mT1.orig.png" :status/picture :status.picture/failed :status.picture/message "500 Internal Server Error"}]
-   ])
+   ["Uploading tall 30%" {:blob/url "/post/0T6kDVGD2/LycvDQV.orig.jpeg" :status/picture :status.picture/uploading :status.picture/progress 0.3}]
+   ["Upload failed"      {:blob/url "/post/0THGrh25y/M0C_mT1.orig.png" :status/picture :status.picture/failed :status.picture/message "500 Internal Server Error"}]
+   ["Upload failed long" {:blob/url "/post/0THGrh25y/M0C_mT1.orig.png" :status/picture :status.picture/failed :status.picture/message "500 Internal Server Error Machine says no No way Nope NaN NaN NaN"}]
+   ["Uploaded"           {:post {:picture {:url "/post/0THGrh25y/M0C_mT1.fit.jpeg", :content-type "image/jpeg", :dimensions [750 265]}}}]
+   ["Uploaded vertical"  {:post {:picture {:url "/post/0TDdxMGm8/M-Jw_2o.fit.jpeg", :content-type "image/jpeg", :dimensions [638 922]}}}]
+])
 
 
 (defonce *debug-state-key (atom "New"))

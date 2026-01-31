7ed5d1f04772cf1307926f2e79ff8adb5502fc91
Nikita Prokopov <niki@tonsky.me>
2026-01-31 22:41:32 +0100

    clj-simple-stats 1.2.0

--- script/java_opts.sh
+++ script/java_opts.sh
@@ -5,7 +5,7 @@ set -o errexit -o nounset -o pipefail
 java_version=$(java -version 2>&1 | head -n 1 | cut -d'"' -f2 | cut -d'.' -f1)
 
 # Build Java options based on version
-opts=""
+opts="-J-Dclojure.main.report=stderr -J-Duser.language=en -J-Duser.country=US -J-Dfile.encoding=UTF-8"
 
 if [ "$java_version" -ge 16 ]; then
   opts="$opts -J--add-modules -Jjdk.incubator.vector"
--- script/repl.sh
+++ script/repl.sh
@@ -2,4 +2,4 @@
 set -o errexit -o nounset -o pipefail
 cd "`dirname $0`/.."
 
-clojure $(./script/java_opts.sh) -M:dev:java -m user
+clojure $(./script/java_opts.sh) -M:dev -m user
